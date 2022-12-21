0da94804ea51ea0be676629edcb7bdf9a602bfdc
veerendra2 <veerendra2@github.com>
2022-12-21 01:00:47 +0000

    36 Fixes restic_backup script

--- apps/nextcloud/restic_backup.sh
+++ apps/nextcloud/restic_backup.sh
@@ -9,45 +9,53 @@
 # Set --collector.textfile.directory in node_exporter
 
 
-RESTIC_REPO="/media/disk1/nextcloud_restic_test"
-# RESTIC_BACKUP_DIR=""
+NEXTCLOUD_DATA_RESTIC_REPO="/media/disk1/nextcloud_data_restic_test"
+NEXTCLOUD_PG_RESTIC_REPO="/media/disk1/nextcloud_pg_restic_test"
+BACKUP_DIR="/media/disk2/nextcloud"
 RESTIC_PASSWORD_FILE="/home/veerendra/pass.txt"
 TEXTFILE_COLLECTOR_DIR="/opt/apps/monitoring/textfile_collector"
 
 START="$(date +%s)"
-trap "docker exec -it nextcloud php occ maintenance:mode --off" EXIT
+trap "docker exec -it -u www-data nextcloud php occ maintenance:mode --off" EXIT
 
-restic -r $RESTIC_REPO \
-      --json \
+echo "[*] Settings maintenance mode on"
+docker exec -it -u www-data nextcloud php occ maintenance:mode --on
+echo "[*] Backup nextcloud data to the restic repo -> $NEXTCLOUD_DATA_RESTIC_REPO"
+restic -r $NEXTCLOUD_DATA_RESTIC_REPO \
       -p $RESTIC_PASSWORD_FILE \
-      backup $RESTIC_BACKUP_DIR | tail -1 > /tmp/restic_stats.json
+      --json \
+      backup $BACKUP_DIR | tail -1 > /tmp/nextcloud_data_restic_stats.json
 
+echo "[*] Backup nextcloud pg to the restic repo -> $NEXTCLOUD_PG_RESTIC_REPO"
 docker exec postgres \
       bash -c 'pg_dump -c -U `cat $POSTGRES_USER_FILE` `cat $POSTGRES_DB_FILE`' | \
       restic backup --stdin --json \
-      -r /home/veerendra/test \
+      -r $NEXTCLOUD_PG_RESTIC_REPO \
       -p /home/veerendra/pass.txt \
-      --stdin-filename db_postgres_nextcloud.sql | tail -1 > /tmp/restic_stats.json
+      --stdin-filename db_postgres_nextcloud.sql | tail -1 > /tmp/nextcloud_pg_restic_stats.json
 
+echo "[*] Settings maintenance mode off"
+docker exec -it -u www-data nextcloud php occ maintenance:mode --off
 trap "" EXIT
 END="$(date +%s)"
-
+echo "AFTER"
+for stat_file in /tmp/{"nextcloud_data_restic_stats.json","nextcloud_data_restic_stats.json"};
+do
 cat << EOF > "$TEXTFILE_COLLECTOR_DIR/backup_restic.prom.$$"
-backup_duration_seconds $(($END - $START))
 backup_last_run_epoch_seconds $END
-backup_restic_summary_files_new `jq .files_new /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_files_changed `jq .files_changed /tmp/restic_stats.json || echo -n ""-1""`
-backup_restic_summary_files_unmodified `jq .files_unmodified /tmp/restic_stats.json  || echo -n "-1"`
-backup_restic_summary_dirs_new `jq .dirs_new /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_dirs_changed `jq .dirs_changed /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_dirs_unmodified `jq .dirs_unmodified /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_data_blobs `jq .data_blobs /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_tree_blobs `jq .tree_blobs /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_data_added `jq .data_added /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_total_files_processed `jq .total_files_processed /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_total_bytes_processed `jq .total_bytes_processed /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_total_duration `jq .total_duration /tmp/restic_stats.json || echo -n "-1"`
-backup_restic_summary_snapshot_id `jq .snapshot_id /tmp/restic_stats.json || echo -n "-1"`
+backup_restic_summary_files_new `jq .files_new $stat_file || echo -n "-1"`
+backup_restic_summary_files_changed `jq .files_changed $stat_file || echo -n ""-1""`
+backup_restic_summary_files_unmodified `jq .files_unmodified $stat_file  || echo -n "-1"`
+backup_restic_summary_dirs_new `jq .dirs_new $stat_file || echo -n "-1"`
+backup_restic_summary_dirs_changed `jq .dirs_changed $stat_file || echo -n "-1"`
+backup_restic_summary_dirs_unmodified `jq .dirs_unmodified $stat_file || echo -n "-1"`
+backup_restic_summary_data_blobs `jq .data_blobs $stat_file || echo -n "-1"`
+backup_restic_summary_tree_blobs `jq .tree_blobs $stat_file || echo -n "-1"`
+backup_restic_summary_data_added `jq .data_added $stat_file || echo -n "-1"`
+backup_restic_summary_total_files_processed `jq .total_files_processed $stat_file || echo -n "-1"`
+backup_restic_summary_total_bytes_processed `jq .total_bytes_processed $stat_file || echo -n "-1"`
+backup_restic_summary_total_duration `jq .total_duration $stat_file || echo -n "-1"`
+backup_restic_summary_snapshot_id `jq .snapshot_id $stat_file || echo -n "-1"`
 EOF
-rm  /tmp/restic_stats.json
 mv "$TEXTFILE_COLLECTOR_DIR/backup_restic.prom.$$" "$TEXTFILE_COLLECTOR_DIR/backup_restic.prom"
+done
