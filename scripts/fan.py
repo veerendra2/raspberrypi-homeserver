26bc613702937f21eab20813660a2b97395f023b
veerendra2 <veerendra2@github.com>
2023-03-08 19:21:07 +0100

    50 Test new fan.py script

--- scripts/fan.py
+++ scripts/fan.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
 '''
 Description: A simple script to control 3pin PWM fan in raspberry pi 4
-Reference: https://github.com/geekworm-com/x-c1/blob/main/fan.py
 '''
 import pigpio
 import time
 import logging
 
+# Pin number for the fan
+FAN_PIN = 18
+
+# PWM frequency and duty cycle range
+PWM_FREQ = 25000
+PWM_RANGE = 100
+
+# Temperature thresholds and corresponding duty cycles
+TEMP_THRESHOLDS = [(75, 100), (70, 80), (60, 70), (48, 50), (30, 40)]
+
 logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', level=logging.DEBUG)
 
-servo = 18
-
 pwm = pigpio.pi()
-pwm.set_mode(servo, pigpio.OUTPUT)
-pwm.set_PWM_frequency(servo, 25000)
-pwm.set_PWM_range(servo, 100)
-while (True):
+pwm.set_mode(FAN_PIN, pigpio.OUTPUT)
+pwm.set_PWM_frequency(FAN_PIN, PWM_FREQ)
+pwm.set_PWM_range(FAN_PIN, PWM_RANGE)
+
+prev_dc = None
+while True:
   # Get CPU temp
   with open("/sys/class/thermal/thermal_zone0/temp") as f:
-    temp = float(f.read()) / 1000.00
-    temp = float('%.2f' % temp)
+      temp = float(f.read()) / 1000.00
+      temp = round(temp, 2)
 
-  if (temp > 75):
-    logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to 100%")
-    pwm.set_PWM_dutycycle(servo, 100)
-  elif (temp > 70):
-    logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to 80%")
-    pwm.set_PWM_dutycycle(servo, 80)
-  elif (temp > 60):
-    logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to 70%")
-    pwm.set_PWM_dutycycle(servo, 70)
-  elif (temp > 50):
-    logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to 50%")
-    pwm.set_PWM_dutycycle(servo, 50)
-  elif (temp > 30):
-    logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to 40%")
-    pwm.set_PWM_dutycycle(servo, 40)
-  elif (temp < 30):
-    logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to 0%")
-    pwm.set_PWM_dutycycle(servo, 0)
+  # Determine duty cycle based on temperature
+  duty_cycle = 0
+  for threshold, dc in TEMP_THRESHOLDS:
+      if temp >= threshold:
+          duty_cycle = dc
+          break
 
-  time.sleep(120)
+  # Set fan speed if duty cycle has changed
+  if duty_cycle != prev_dc:
+      pwm.set_PWM_dutycycle(FAN_PIN, duty_cycle)
+      logging.debug(f"Current CPU temperature is {temp}°C. Setting fan speed to {duty_cycle}%")
+      prev_dc = duty_cycle
+
+  # Wait for 2 minutes before checking temperature again
+  time.sleep(60)
